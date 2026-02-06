3d4718489a0d1674b73c34b9ee41e40175f464d2
Rimvydas Kersys <r.kersys@lancaster.ac.uk>
2026-02-06 11:50:06 +0000

    disable logger

--- testRP/test_rp.py
+++ testRP/test_rp.py
@@ -42,4 +42,5 @@ def video_feed():
 	return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
+	logger.disabled = True
 	app.run(host='0.0.0.0', port = 5000, debug = True, threaded = True)
